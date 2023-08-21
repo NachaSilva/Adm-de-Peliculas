8a36220e9359b93639dc038d075f50b1dc5a3d79
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-08-21 16:26:58 -0400

    Se ve en interfaz las peliculas que añade el usuario

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,39 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 export const Listado = () => {
+
+  const [listadoState, setListadoState] = useState([]);
+
+  useEffect(()=>{
+    console.log('componentes del listado de pelis')
+  conseguirPeliculas();
+  
+  }, []);
+
+  const conseguirPeliculas = () => {
+    let peliculas = JSON.parse(localStorage.getItem('movies'));
+    
+    setListadoState(peliculas);
+  };
+
   return (
-  <>
-   <article className='peli-item'>
-          <h3 className='title'>Desarrollo web</h3>
-          <p className='description'>victorroblesweb.es</p>
+    <>
+  {listadoState !=null ? listadoState.map(movie => {
+    
+    return(
+      <article key={movie.id} className='peli-item'>
+        <h3 className='title'>{movie.title}</h3>
+        <p className='description'>{movie.description}</p>
 
-          <button className='edit'>Editar</button>
-          <button className='delete'>Borrar</button>
-        </article>
-
-        <article className='peli-item'>
-          <h3 className='title'>Desarrollo web</h3>
-          <p className='description'>victorroblesweb.es</p>
-
-          <button className='edit'>Editar</button>
-          <button className='delete'>Borrar</button>
-        </article>
-
-        <article className='peli-item'>
-          <h3 className='title'>Desarrollo web</h3>
-          <p className='description'>victorroblesweb.es</p>
-
-          <button className='edit'>Editar</button>
-          <button className='delete'>Borrar</button>
-        </article>
-
-        <article className='peli-item'>
-          <h3 className='title'>Desarrollo web</h3>
-          <p className='description'>victorroblesweb.es</p>
-
-          <button className='edit'>Editar</button>
-          <button className='delete'>Borrar</button>
-        </article>
-  </>
-  )
+        <button className='edit'>Editar</button>
+        <button className='delete'>Borrar</button>
+      </article>
+    )
+  })
+: <h2>No hay peliculas para mostrar</h2>
 }
+
+
+    </>
+  );
+};
